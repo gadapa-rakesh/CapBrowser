92cb18bfdcc3cc36553e5f530b1e1bf90cb73056
Rakesh Rao Gadapa <rakeshraogadapa@Rakeshs-MacBook-Pro.local>
2020-04-25 13:57:20 -0400

    updating typescript interface & return close result

--- src/definitions.ts
+++ src/definitions.ts
@@ -4,6 +4,42 @@ declare module "@capacitor/core" {
   }
 }
 
+enum ToolBarType {
+  ACTIVITY = "activity",
+  NAVIGATION = "navigation",
+  BLANK = "blank",
+  DONE = "done",
+}
+
+export interface Headers {
+  [key: string] : string;
+}
+
+export interface OpenOptions {
+  url: string;
+  headers?: Headers;
+  isFullScreenPresentationStyle?: boolean;
+}
+
+export interface DisclaimerOptions {
+  title: string;
+  message: string;
+  confirmBtn: string;
+  cancelBtn: string;
+}
+
+export interface OpenWebViewOptions {
+  url: string;
+  headers?: Headers;
+  shareDisclaimer?: DisclaimerOptions;
+  toolbarType?: ToolBarType;
+  shareSubject?: string;
+  title: string;
+  isFullScreenPresentationStyle?: boolean;
+}
+
 export interface CapBrowserPlugin {
-  open(options: { value: string }): Promise<{value: string}>;
+  open(options: OpenOptions): Promise<any>;
+  close(): Promise<any>;
+  openWebView(options: OpenWebViewOptions): Promise<any>;
 }
